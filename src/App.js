df44795d0d2670f9a5e037ced356e158c368c486
Erik Flogvall <erik@flogvall.com>
2021-01-02 14:42:10 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Erik är bäst! I test! oh yeah ochasd as
         </p>
         <a
           className="App-link"
